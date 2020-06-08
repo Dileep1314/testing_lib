06c24968469217c7dea1ae0c71c7fb21e9e8012c
Dileep1314 <55228534+Dileep1314@users.noreply.github.com>
2020-06-08 21:21:08 +0530

    Update cleanup.groovy

--- vars/cleanup.groovy
+++ vars/cleanup.groovy
@@ -1,10 +1,12 @@
 #!/usr/bin/env groovy
 //variables for npm_registry,nexus_url_imagine is added in global variables.
 def call(body) {
+    node{
     script{
     sh '''
     echo "Running npm clean"
        '''
  return this
+}
 }
 }
